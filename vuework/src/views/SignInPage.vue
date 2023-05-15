92ad2a4d688f9e1f736674f46b4e62cd616336ee
cailynkit <113132191+cailynkit@users.noreply.github.com>
2023-05-15 13:59:29 -0400

    sign in page

---
+++ vuework/src/views/SignInPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+ <div>
+    <input type="text" placeholder="Username" />
+    <input type="text" placeholder="Password" />
+    <button>Log In</button>
+    <button>Create Account</button>
+ </div>
+</template>
