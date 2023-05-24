5b3309d971feea21b99b77105750b6fee466c65a
cailynkit <113132191+cailynkit@users.noreply.github.com>
2023-05-24 13:33:10 -0400

    bad side bar

--- vuework/src/components/sideBarSection.vue
+++ vuework/src/components/sideBarSection.vue
@@ -5,13 +5,13 @@
 <template>
 <div>
     <h1>Lines</h1>
-    <div id="lineoptions">
+    <div class="options">
         <p id="test">dfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfddfdfd</p>
     </div>
     <br><br><br><br><br><br>
     <h1>Shapes</h1>
-    <div id="lineoptions">
-        <p id="test">dfdfd</p>
+    <div class="options">
+        <p id="test"></p>
     </div>
 
 </div>
@@ -23,11 +23,11 @@ h1 {
     border-bottom: 2px solid black;
 }
 
-#lineoptions {
+.options {
     width: 20vw;
     height: 100px;
     overflow-y: hidden;
-    overflow-x: scroll;
+    overflow-x: auto;
 }
 
 </style>
